dba508ab5e19f0cbe04468fd56a901826807a5c2
c-auri <43008483+c-auri@users.noreply.github.com>
2024-02-24 20:18:25 +0100

    Store teas as json files

--- src/api/Tea/TeaRepository.cs
+++ src/api/Tea/TeaRepository.cs
@@ -1,78 +1,32 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text.Json;
+
 namespace api;
 
 public class TeaRepository 
-{
-    private static readonly Tea[] teas = new[]
-    {
-        new Tea(
-            1,
-            "Pretty Girls Mini",
-            2021,
-            "https://white2tea.com/products/2021-pretty-girls-mini"),
-        new Tea(
-            2,
-            "Smokeshou Mini",
-            2021,
-            "https://white2tea.com/products/2021-smokeshou-mini"),
-        new Tea(
-            3,
-            "Hokum Mini",
-            2021,
-            "https://white2tea.com/products/2021-hokum-minis"),
-        new Tea(
-            4,
-            "Lesser Evils Mini",
-            2021,
-            "https://white2tea.com/products/2021-lesser-evils-minis"),
-        new Tea(
-            5,
-            "Tale Chaser Mini",
-            2022,
-            "https://white2tea.com/products/2022-tale-chaser-minis"),
-        new Tea(
-            6,
-            "Lumber Mini",
-            2022,
-            "https://white2tea.com/products/2022-lumber-minis"),
-        new Tea(
-            7,
-            "Predawn Dark Mini",
-            2023,
-            "https://white2tea.com/products/2023-predawn-dark-minis"),
-        new Tea(
-            8,
-            "Stubb Mini", 
-            2023,
-            "https://white2tea.com/products/2023-stubb-minis"),
-        new Tea(
-            9,
-            "Prosbloom Mini",
-            2023,
-            "https://white2tea.com/products/2023-prosbloom-mini"),
-        new Tea(
-            10,
-            "Big O V3 Mini",
-            2023,
-            "https://white2tea.com/products/2023-big-o-v3-mini"),
-        new Tea(
-            11,
-            "Reading Room Mini", 
-            2023,
-            "https://white2tea.com/products/2023-reading-room-mini"),
-        new Tea(
-            12,
-            "Red Loon Mini",
-            2023,
-            "https://white2tea.com/products/2023-red-loon-mini"),
-        new Tea(
-            13,
-            "Glass House Mini",
-            2023,
-            "https://white2tea.com/products/2023-glass-house-mini"),
-    };
+{ 
+   internal IEnumerable<Tea> Get()
+   {
+      var result = new List<Tea>();
+    
+      foreach (var file in Directory.GetFiles("../../data/teas/"))
+      {
+         result.Add(Deserialize(file));
+      }
 
-    internal IEnumerable<Tea> Get()
-    {
-        return teas;
-    }
+      return result; 
+   }
+
+   private Tea Deserialize(string file)
+   {
+      var options = new JsonSerializerOptions()
+      {
+         PropertyNameCaseInsensitive = true,
+      };
+
+      var json = File.ReadAllText(file);
+      return JsonSerializer.Deserialize<Tea>(json, options);
+   }
 }
