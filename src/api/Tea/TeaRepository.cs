d2397953b07fb75895c9fb37ec702963ea8c5c55
c-auri <43008483+c-auri@users.noreply.github.com>
2024-02-24 20:20:03 +0100

    Refactor Deserialization

--- src/api/Tea/TeaRepository.cs
+++ src/api/Tea/TeaRepository.cs
@@ -7,26 +7,17 @@ namespace api;
 
 public class TeaRepository 
 { 
+   private const string teaStorePath = "../../data/teas/";
+
    internal IEnumerable<Tea> Get()
    {
-      var result = new List<Tea>();
-    
-      foreach (var file in Directory.GetFiles("../../data/teas/"))
-      {
-         result.Add(Deserialize(file));
-      }
-
-      return result; 
+      return Directory.GetFiles(teaStorePath).Select(Deserialize); 
    }
 
    private Tea Deserialize(string file)
    {
-      var options = new JsonSerializerOptions()
-      {
-         PropertyNameCaseInsensitive = true,
-      };
-
-      var json = File.ReadAllText(file);
-      return JsonSerializer.Deserialize<Tea>(json, options);
+      return JsonSerializer.Deserialize<Tea>(
+            File.ReadAllText(file), 
+            new JsonSerializerOptions() { PropertyNameCaseInsensitive = true });
    }
 }
